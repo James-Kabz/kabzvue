380e88674ebe091ed33c55a9ac65a5211eacd03e
James-Kabz <kabogp@gmail.com>
2026-04-22 15:22:19 +0300

    fix: tab panel fix

--- src/components/TabPanel.vue
+++ src/components/TabPanel.vue
@@ -6,7 +6,7 @@
     :aria-labelledby="`tab-${index}`"
     class="ui-text"
   >
-    <div class="rounded-xl border ui-border bg-(--ui-surface) p-4 md:p-5 shadow-xs">
+    <div class="rounded-xl border ui-border bg-(--ui-surface) p-2 shadow-xs">
       <div
         v-if="loading"
         class="flex items-center justify-center p-8"
